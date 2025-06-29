d2c4517d28a22e2678494e733d033b744fdfdcad
HoriK816 <horishi.root2@gmail.com>
2025-06-29 23:39:53 +0900

    remove old macros

--- src/history.h
+++ src/history.h
@@ -2,9 +2,5 @@
 
 #define HISTORY_PATH "../logs/history.log"
 
-#define HISTORY_PATH_WSL "/home/horik/develop/ShellModoki/history.log"
-#define HISTORY_PATH_MAC "../logs/history.log"
-#define HISTORY_PATH_EXP_ENV "/2024/v24e3026/develop/ShellModoki/history.log"
-
 void RecordCommandHistory(char* command);
 void PrintHistory();
